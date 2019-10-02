9fee9c013186f5425b356d8db2a1927c6e56b6a0
Andy Lech <github@andylech.com>
2019-10-02 11:07:00 -0400

    Add empty HomePage and HomePageModel

---
+++ SwapiMvvm.Forms/SwapiMvvm.Forms/Pages/HomePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using SwapiMvvm.Forms.PageModels;
+using SwapiMvvm.Forms.Services.Messaging;
+
+namespace SwapiMvvm.Forms.Pages
+{
+    public partial class HomePage : BasePage<HomePageModel>
+    {
+        #region Constructors
+
+        public HomePage()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception exception)
+            {
+                App.MessagingService.SendErrorMessage(exception);
+            }
+        }
+
+        #endregion
+
+        #region Interface
+
+        #endregion
+
+        #region Protected Overrides
+
+        #endregion
+
+        #region Bindable Properties
+
+        #endregion
+
+        #region Events
+
+        #endregion
+
+        #region Private
+
+        #endregion
+    }
+}
