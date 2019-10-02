193c960ea677a1eeccdac497d343bdefcaa71f51
Andy Lech <github@andylech.com>
2019-10-02 11:25:19 -0400

    Add missing AppSections to NavigationState

--- SwapiMvvm.Forms/SwapiMvvm.Forms.Services/Models/NavigationState.cs
+++ SwapiMvvm.Forms/SwapiMvvm.Forms.Services/Models/NavigationState.cs
@@ -9,11 +9,13 @@ namespace SwapiMvvm.Forms.Services.Models
 
         public enum AppSection
         {
+            Splash,
             Home,
             Films,
             People,
             Species,
-            About
+            About,
+            Modal
         }
 
         #endregion
