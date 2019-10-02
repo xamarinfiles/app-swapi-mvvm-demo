8faed1973fb63662e96a21bfe331c11a85dba342
Andy Lech <github@andylech.com>
2019-10-02 11:10:53 -0400

    Add Dimension styles

---
+++ SwapiMvvm.Forms/SwapiMvvm.Forms/Styles/Dimensions.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SwapiMvvm.Forms.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Dimensions : ResourceDictionary
+    {
+        public Dimensions()
+        {
+            InitializeComponent();
+        }
+    }
+}
