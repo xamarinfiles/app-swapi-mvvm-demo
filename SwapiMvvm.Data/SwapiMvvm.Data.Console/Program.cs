28e33c70b4144b5736b40b0f4d8af23f870fb3a5
Andy Lech <github@andylech.com>
2019-10-03 11:46:28 -0400

    Add empty data project and references

---
+++ SwapiMvvm.Data/SwapiMvvm.Data.Console/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using FancyLogger;
+
+namespace SwapiMvvm.Data.Console
+{
+    public class Program
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                LoggingService = new FancyLoggerService();
+
+                LoggingService.WriteLine("Hello World!");
+            }
+            catch (Exception exception)
+            {
+                LoggingService?.WriteException(exception);
+            }
+        }
+
+        #endregion
+
+        #region Public
+
+        public static FancyLoggerService LoggingService { get; private set; }
+
+        #endregion
+
+        #region Interface
+
+        #endregion
+
+        #region Protected
+
+        #endregion
+
+        #region Internal
+
+        #endregion
+
+        #region Private
+
+        #endregion
+
+        #region Nested Types
+
+        #endregion
+    }
+}
