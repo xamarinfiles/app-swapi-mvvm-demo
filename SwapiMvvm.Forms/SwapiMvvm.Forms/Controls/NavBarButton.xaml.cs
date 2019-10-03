9c1317223d96f95c97d0c3094c70b484833bb094
Andy Lech <github@andylech.com>
2019-10-03 10:57:36 -0400

    Add page controls

---
+++ SwapiMvvm.Forms/SwapiMvvm.Forms/Controls/NavBarButton.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using SwapiMvvm.Forms.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SwapiMvvm.Forms.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavBarButton : ContentView
+    {
+        #region Enums
+
+        #endregion
+
+        #region Constructors
+
+        public NavBarButton()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception exception)
+            {
+                App.MessagingService.SendErrorMessage(exception);
+            }
+        }
+
+        #endregion
+
+        #region Protected Overrides
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            try
+            {
+                var navItem = (FooterNavItem) BindingContext;
+
+                if (!navItem.IsSelected)
+                    return;
+
+                NavItemName.FontAttributes = FontAttributes.Bold;
+                // TODO Replace this
+                Application.Current.Resources.TryGetValue("SelectedNavButtonBackground",
+                    out var backgroundColor);
+                if (backgroundColor != null)
+                    This.BackgroundColor = (Color) backgroundColor;
+            }
+            catch (Exception exception)
+            {
+                App.MessagingService.SendErrorMessage(exception);
+            }
+        }
+
+        #endregion
+
+        #region Bindable Properties
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Events
+
+        #endregion
+
+        #region Delegates
+
+        #endregion
+
+        #region Private
+
+        #endregion
+    }
+}
---
+++ SwapiMvvm.Forms/SwapiMvvm.Forms/Controls/NavBarFooter.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using SwapiMvvm.Forms.PageModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SwapiMvvm.Forms.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavBarFooter : ContentView
+    {
+        #region Constructors
+
+        public NavBarFooter()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception exception)
+            {
+                App.MessagingService.SendErrorMessage(exception);
+            }
+        }
+
+        #endregion
+
+        //#region Protected Overrides
+
+        //protected override void OnBindingContextChanged()
+        //{
+        //    base.OnBindingContextChanged();
+
+        //    BasePageModel pageModel = (BasePageModel) BindingContext;
+        //}
+
+        //#endregion
+
+        #region Bindable Properties
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Events
+
+        #endregion
+
+        #region Delegates
+
+        #endregion
+
+        #region Private
+
+        #endregion
+    }
+}
